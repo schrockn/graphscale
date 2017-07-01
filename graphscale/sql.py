0def45e7ffa4a81ad788db2fe3f5fdb153c79225
Nicholas Schrock <schrockn@gmail.com>
2017-06-30 17:15:44 -0700

    add create_conn_info

--- graphscale/sql.py
+++ graphscale/sql.py
@@ -1,8 +1,18 @@
 from collections import namedtuple
 import pymysql
+import pymysql.cursors
+
 from graphscale import check
 
 ConnectionInfo = namedtuple('ConnectionInfo', 'host user password db charset cursorclass')
+
+
+def create_conn_info(
+    *, host, user, password, db, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor
+):
+    return ConnectionInfo(
+        host=host, user=user, password=password, db=db, charset=charset, cursorclass=cursorclass
+    )
 
 
 def pymysql_conn_from_info(conn_info):
