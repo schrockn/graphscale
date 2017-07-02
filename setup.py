85f818eb3b36a101f3f7c23312d0473073e6dd9c
Nicholas Schrock <schrockn@gmail.com>
2017-07-02 10:41:13 -0700

    first stab at setup.py

--- setup.py
+++ setup.py
@@ -1,72 +1,37 @@
-import sys
-
 from setuptools import find_packages, setup
-
-# cargo-culting
-if sys.version_info[0] < 3:
-    import __builtin__ as builtins
-else:
-    import builtins
-
-# ???? uncomment once python versioning system is understood
-# version = __import__('graphene').get_version()
-
-tests_require = [
-    'pytest>=2.7.2',
-    # 'pytest-benchmark',
-    # 'pytest-cov',
-    # 'snapshottest',
-    # 'coveralls',
-    # 'six',
-    # 'mock',
-    # 'pytz',
-    # 'iso8601',
-]
 
 setup(
     name='graphscale',
-    #version=version,
-
-    # description='GraphQL Framework for Python',
+    version='0.0.1',
+    description='GraphQL API Server Framework for Python',
     # long_description=open('README.rst').read(),
     url='https://github.com/schrockn/graphscale',
     author='Nicholas Schrock',
     author_email='schrockn@gmail.com',
     license='MIT',
-
-    #classifiers=[
-    #    'Development Status :: 3 - Alpha',
-    #    'Intended Audience :: Developers',
-    #    'Topic :: Software Development :: Libraries',
-    #    'Programming Language :: Python :: 2',
-    #    'Programming Language :: Python :: 2.7',
-    #    'Programming Language :: Python :: 3',
-    #    'Programming Language :: Python :: 3.3',
-    #    'Programming Language :: Python :: 3.4',
-    #    'Programming Language :: Python :: 3.5',
-    #    'Programming Language :: Python :: Implementation :: PyPy',
-    #],
-
-    #keywords='api graphql protocol rest relay graphene',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Environment :: MacOS X',
+    ],
+    keywords='api graphql protocol rest relay graphscale',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'pymysql',
-        'six',
-        'graphql-core',
-        'redis',
-        'iso8601',
-        #    'graphql-relay>=0.4.5',
-        #    'promise>=2.0',
+        'aiodataloader>=0.1.2',
+        'graphql-core>=1.1',
+        'iso8601>=0.1.11',
+        'promise>=2.0.2',
+        'py>=1.4.34',
+        'PyMySQL>=0.7.11',
+        'redis>=2.10.5',
+        'six>=1.10.0',
+        'typing>=3.6.1',
     ],
-    # tests_require=tests_require,
-    # extras_require={
-    #     'test': tests_require,
-    #     #'django': [
-    #     #    'graphene-django',
-    #     #],
-    #     #'sqlalchemy': [
-    #     #    'graphene-sqlalchemy',
-    #     #]
-    # },
-    # cmdclass={'test': PyTest},
+    tests_require=[
+        'pytest>=3.1.2',
+        'pytest-asyncio>=0.6.0',
+    ],
 )
