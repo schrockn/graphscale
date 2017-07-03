d0bdd50a8ca6367b08434075ab9573f5e412a197
Nicholas Schrock <schrockn@gmail.com>
2017-07-03 08:02:36 -0700

    typeify dbschema

--- graphscale/kvetch/dbschema.py
+++ graphscale/kvetch/dbschema.py
@@ -1,20 +1,22 @@
 from warnings import filterwarnings, resetwarnings
 import contextlib
+
+from typing import Iterator, List
 import pymysql
 
-import graphscale.check as check
 from graphscale.utils import execute_gen
 from .kvetch import IndexDefinition, IndexType
+from .dbshard import KvetchDbShard
 
 
 @contextlib.contextmanager
-def disable_pymysql_warnings():
-    filterwarnings('ignore', category=pymysql.Warning)
+def disable_pymysql_warnings() -> Iterator:
+    filterwarnings('ignore', category=pymysql.Warning)  # type: ignore
     yield
     resetwarnings()
 
 
-def execute_ddl(shard, ddl):
+def execute_ddl(shard: KvetchDbShard, ddl: str) -> str:
     with disable_pymysql_warnings():
         with shard.create_safe_conn() as conn:
             with conn.cursor() as cursor:
@@ -22,7 +24,7 @@ def execute_ddl(shard, ddl):
     return ddl
 
 
-def create_kvetch_objects_table_sql():
+def create_kvetch_objects_table_sql() -> str:
     return """CREATE TABLE IF NOT EXISTS kvetch_objects (
     row_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     obj_id BINARY(16) NOT NULL,
@@ -37,10 +39,9 @@ def create_kvetch_objects_table_sql():
 """
 
 
-def create_kvetch_index_table_sql(index_column, index_sql_type, target_column, index_name):
-    check.param(index_column, str, 'index_column')
-    check.param(target_column, str, 'target_column')
-    check.param(index_name, str, 'index_name')
+def create_kvetch_index_table_sql(
+    index_column: str, index_sql_type: str, target_column: str, index_name: str
+) -> str:
 
     # something is up here. the two indexing keys (not updated) should be unique
     return """CREATE TABLE IF NOT EXISTS %s (
@@ -58,7 +59,7 @@ def create_kvetch_index_table_sql(index_column, index_sql_type, target_column, i
     )
 
 
-def create_kvetch_edge_table_sql():
+def create_kvetch_edge_table_sql() -> str:
     return """CREATE TABLE IF NOT EXISTS kvetch_edges (
     row_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     edge_id INT NOT NULL,
@@ -74,17 +75,15 @@ def create_kvetch_edge_table_sql():
 """
 
 
-def create_kvetch_objects_table(shard):
+def create_kvetch_objects_table(shard: KvetchDbShard) -> None:
     execute_ddl(shard, create_kvetch_objects_table_sql())
 
 
-def create_kvetch_edges_table(shard):
+def create_kvetch_edges_table(shard: KvetchDbShard) -> None:
     execute_ddl(shard, create_kvetch_edge_table_sql())
 
 
-def create_kvetch_index_table(shard, shard_index):
-    check.param(shard_index, IndexDefinition, 'shard_index')
-
+def create_kvetch_index_table(shard: KvetchDbShard, shard_index: IndexDefinition):
     mapping = {
         IndexType.STRING: 'VARCHAR(512)',
         IndexType.INT: 'INT',
@@ -97,26 +96,15 @@ def create_kvetch_index_table(shard, shard_index):
     execute_ddl(shard, sql)
 
 
-def init_shard_db_tables(shard, indexes):
-    check.param(indexes, list, 'indexes')
+def init_shard_db_tables(shard: KvetchDbShard, indexes: List[IndexDefinition]):
     create_kvetch_objects_table(shard)
     create_kvetch_edges_table(shard)
     for shard_index in indexes:
         create_kvetch_index_table(shard, shard_index)
 
 
-def drop_shard_db_tables(shard, indexes):
-    check.param(indexes, list, 'indexes')
+def drop_shard_db_tables(shard: KvetchDbShard, indexes: List[IndexDefinition]):
     execute_ddl(shard, 'DROP TABLE IF EXISTS kvetch_objects')
     execute_ddl(shard, 'DROP TABLE IF EXISTS kvetch_edges')
     for shard_index in indexes:
         execute_ddl(shard, 'DROP TABLE IF EXISTS %s' % shard_index.index_name)
-
-
-def build_index(shard, index):
-    create_kvetch_index_table(shard, index)
-    objects = execute_gen(shard.gen_objects_of_type(index.indexed_type_id))
-    attr = index.indexed_attr
-    for obj_id, obj in objects.items():
-        if attr in obj:
-            execute_gen(shard.gen_insert_index_entry(index, obj[attr], obj_id))
