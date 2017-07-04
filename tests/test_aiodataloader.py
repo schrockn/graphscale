bf29b2647dcaf3ae4ab8f291b08af100ab23c281
Nicholas Schrock <schrockn@gmail.com>
2017-07-04 10:49:11 -0700

    cp

--- tests/test_aiodataloader.py
+++ tests/test_aiodataloader.py
@@ -1,25 +1,24 @@
+from typing import List
 import pytest
 import asyncio
 from aiodataloader import DataLoader
 
+from graphscale.utils import async_list
+
 pytestmark = pytest.mark.asyncio
 
 
-async def async_list(seq):
-    return await asyncio.gather(*seq)
-
-
 class StringLoader(DataLoader):
-    def __init__(self):
-        super().__init__()
-        self.calls = []
+    def __init__(self) -> None:
+        super().__init__(batch_load_fn=self._batch_load_fn)
+        self.calls = []  # type: List[List[int]]
 
-    async def batch_load_fn(self, keys):
+    async def _batch_load_fn(self, keys: List[int]) -> List[str]:
         self.calls.append(keys)
         return [str(key) for key in keys]
 
 
-async def test_aiodataloader_singleload():
+async def test_aiodataloader_singleload() -> None:
     loader = StringLoader()
     promise1 = loader.load(1)
 
@@ -28,28 +27,28 @@ async def test_aiodataloader_singleload():
     assert loader.calls == [[1]]
 
 
-async def test_aiodataloder_manyload():
+async def test_aiodataloder_manyload() -> None:
     loader = StringLoader()
     values = await loader.load_many([1, 2])
     assert values == ['1', '2']
     assert loader.calls == [[1, 2]]
 
 
-async def test_aiodataloader_loadparallel():
+async def test_aiodataloader_loadparallel() -> None:
     loader = StringLoader()
     values = await asyncio.gather(loader.load(1), loader.load(2))
     assert values == ['1', '2']
     assert loader.calls == [[1, 2]]
 
 
-async def test_aiodataloader_asynclist():
+async def test_aiodataloader_asynclist() -> None:
     loader = StringLoader()
     values = await async_list([loader.load(1), loader.load(2)])
     assert values == ['1', '2']
     assert loader.calls == [[1, 2]]
 
 
-async def test_caching():
+async def test_caching() -> None:
     loader = StringLoader()
     values = await asyncio.gather(loader.load(1), loader.load(2))
     assert values == ['1', '2']
